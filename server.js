76060a94179d69d5caf4c0f2e49b2974751e93be
Julia Evans <julia@jvns.ca>
2017-10-29 22:14:40 -0400

    Update env and some of the website text

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.use(serveStatic('static'))
 app.use(require('morgan')('combined'));
 app.use(require('cookie-parser')());
 app.use(require('body-parser').urlencoded({ extended: true }));
-app.use(require('express-session')({ secret: process.env.SESSION_SECRET2, resave: true, saveUninitialized: true }));
+app.use(require('express-session')({ secret: process.env.SESSION_SECRET, resave: true, saveUninitialized: true }));
 
 // Initialize Passport and restore authentication state, if any, from the
 // session.
